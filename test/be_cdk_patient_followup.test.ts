193fb101704b9743b2c2e98683db412b14030608
Igor Alves <igoralves1@gmail.com>
2024-07-12 19:48:18 -0400

    first commit

---
+++ test/be_cdk_patient_followup.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BeCdkPatientFollowup from '../lib/be_cdk_patient_followup-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/be_cdk_patient_followup-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BeCdkPatientFollowup.BeCdkPatientFollowupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
